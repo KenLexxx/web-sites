51ac14b2483c2adbf757079041850ef1b9353189
KenLexxx <149324767+KenLexxx@users.noreply.github.com>
2024-09-24 13:03:43 +0700

    Add files via upload

---
+++ Новая папка/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/origin.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/main.css">
+    <title>GameNews</title>
+</head>
+<body>
+    <div class="page">
+        <img class="bg" src="img/back.jpg" alt="">
+        <nav>
+            <div class="nav">
+                <div class="hight_nav">
+                    <div class="nav_logo">
+                        <img class="logo" src="img/joy.png" alt="">
+                    </div>
+                    <div class="btn_log">
+                        <a href="#">Вход</a>
+                    </div>
+                    <div class="btn_reg">
+                        <a href="#">Регистрация</a>
+                    </div>
+                </div>
+                <div class="middle_nav">
+                    <div>Каталог</div>
+                    <div class="pc_menu">
+                    <ul class="menu">
+                        <li><a href="index.php">Главная</a></li>
+                        <li><a href="#">О нас</a></li>
+                        <li><a href="#">Новости</a></li>
+                    </ul>
+                    </div>
+                </div>
+                <div class="category_nav">
+
+                </div>
+            </div>
+        </nav>
+    </div>
+</body>
+</html>
+
+<?php
+
+    
+
+?>
